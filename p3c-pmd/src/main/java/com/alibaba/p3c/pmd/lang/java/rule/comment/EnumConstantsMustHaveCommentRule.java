81b8889bdb2412e78a4d65ccba92f7753b6738a1
liuxining02 <liuxining02@zhuanzhuan.com>
2025-03-11 18:01:09 +0800

    Refactor EnumConstantsMustHaveCommentRule to support PMD7

--- p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/comment/EnumConstantsMustHaveCommentRule.java
+++ p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/comment/EnumConstantsMustHaveCommentRule.java
@@ -1,80 +1,72 @@
-/*
- * Copyright 1999-2017 Alibaba Group.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.alibaba.p3c.pmd.lang.java.rule.comment;
 
-import java.util.List;
-import java.util.Map.Entry;
-import java.util.SortedMap;
-import java.util.TreeMap;
-
 import com.alibaba.p3c.pmd.I18nResources;
-import com.alibaba.p3c.pmd.lang.java.rule.util.NodeSortUtils;
-
-import net.sourceforge.pmd.lang.ast.Node;
-import net.sourceforge.pmd.lang.java.ast.ASTCompilationUnit;
 import net.sourceforge.pmd.lang.java.ast.ASTEnumConstant;
 import net.sourceforge.pmd.lang.java.ast.ASTEnumDeclaration;
+import net.sourceforge.pmd.lang.java.ast.JavadocComment;
+
+import java.util.Objects;
 
 /**
  * [Mandatory] All enumeration type fields should be commented as Javadoc style.
  *
- * @author keriezhang
- * @date 2016/12/14
+ * @author XiNing.Liu
+ * @date 2025/03/11
  */
 public class EnumConstantsMustHaveCommentRule extends AbstractAliCommentRule {
 
+
+    /**
+     * Visits enum declarations to check if they have proper Javadoc comments.
+     * Reports violations if the enum declaration is missing a Javadoc comment
+     * or if the Javadoc comment is empty.
+     *
+     * @param enumDeclaration the enum declaration node being visited
+     * @param data context information
+     * @return the result of the visit
+     */
     @Override
-    public Object visit(ASTCompilationUnit cUnit, Object data) {
-        SortedMap<Integer, Node> itemsByLineNumber = this.orderedCommentsAndEnumDeclarations(cUnit);
+    public Object visit(ASTEnumDeclaration enumDeclaration, Object data) {
 
-        // Check comments between ASTEnumDeclaration and ASTEnumConstant.
-        boolean isPreviousEnumDecl = false;
 
-        for (Entry<Integer, Node> entry : itemsByLineNumber.entrySet()) {
-            Node value = entry.getValue();
+        JavadocComment javadocComment = enumDeclaration.getJavadocComment();
+        if (Objects.isNull(javadocComment)) {
+            addViolationWithMessage(data, enumDeclaration,
+                    I18nResources.getMessage("java.comment.EnumConstantsMustHaveCommentRule.violation.msg", enumDeclaration.getSimpleName()));
+        }
+        if (Objects.nonNull(javadocComment) && !javadocComment.hasJavadocContent()) {
+            addViolationWithMessage(data, enumDeclaration,
+                    I18nResources.getMessage("java.comment.EnumConstantsMustHaveCommentRule.violation.msg", enumDeclaration.getSimpleName()));
+        }
+        return super.visit(enumDeclaration, data);
+    }
 
-            if (value instanceof ASTEnumDeclaration) {
-                isPreviousEnumDecl = true;
-            } else if (value instanceof ASTEnumConstant && isPreviousEnumDecl) {
-                Node enumBody = value.jjtGetParent();
-                Node enumDeclaration = enumBody.jjtGetParent();
-                addViolationWithMessage(data, enumBody,
-                    I18nResources.getMessage("java.comment.EnumConstantsMustHaveCommentRule.violation.msg",
-                        enumDeclaration.getImage()));
-                isPreviousEnumDecl = false;
-            } else {
-                isPreviousEnumDecl = false;
-            }
+
+    /**
+     * Visits enum constants to check if they have proper Javadoc comments.
+     * Reports violations if the enum constant is missing a Javadoc comment
+     * or if the Javadoc comment is empty.
+     *
+     * @param enumConstant the enum constant node being visited
+     * @param data context information
+     * @return the result of the visit
+     */
+    @Override
+    public Object visit(ASTEnumConstant enumConstant, Object data) {
+
+        JavadocComment javadocComment = enumConstant.getJavadocComment();
+        if (Objects.isNull(javadocComment)) {
+            addViolationWithMessage(data, enumConstant,
+                    I18nResources.getMessage("java.comment.EnumConstantsMustHaveCommentRule.violation.msg", enumConstant.getName()));
+        }
+        if (Objects.nonNull(javadocComment) && !javadocComment.hasJavadocContent()) {
+            addViolationWithMessage(data, enumConstant,
+                    I18nResources.getMessage("java.comment.EnumConstantsMustHaveCommentRule.violation.msg", enumConstant.getName()));
         }
 
-        return super.visit(cUnit, data);
+        return super.visit(enumConstant, data);
     }
 
-    private SortedMap<Integer, Node> orderedCommentsAndEnumDeclarations(ASTCompilationUnit cUnit) {
-        SortedMap<Integer, Node> itemsByLineNumber = new TreeMap<>();
-
-        List<ASTEnumDeclaration> enumDecl = cUnit.findDescendantsOfType(ASTEnumDeclaration.class);
-        NodeSortUtils.addNodesToSortedMap(itemsByLineNumber, enumDecl);
-
-        List<ASTEnumConstant> contantDecl = cUnit.findDescendantsOfType(ASTEnumConstant.class);
-        NodeSortUtils.addNodesToSortedMap(itemsByLineNumber, contantDecl);
-
-        NodeSortUtils.addNodesToSortedMap(itemsByLineNumber, cUnit.getComments());
-
-        return itemsByLineNumber;
-    }
 
 }
