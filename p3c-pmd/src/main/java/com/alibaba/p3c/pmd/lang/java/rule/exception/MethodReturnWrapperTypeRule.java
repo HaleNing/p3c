7aa330066d5676c0beaf58802ea670e9a02db785
justinLiu <xining.l.se@gmail.com>
2025-03-13 11:31:21 +0800

    Refactor MethodReturnWrapperTypeRule file to support PMD7

--- p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/exception/MethodReturnWrapperTypeRule.java
+++ p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/exception/MethodReturnWrapperTypeRule.java
@@ -1,110 +1,94 @@
-/*
- * Copyright 1999-2017 Alibaba Group.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.alibaba.p3c.pmd.lang.java.rule.exception;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import com.alibaba.p3c.pmd.I18nResources;
 import com.alibaba.p3c.pmd.lang.java.rule.AbstractAliRule;
 import com.alibaba.p3c.pmd.lang.java.util.ViolationUtils;
 import com.alibaba.p3c.pmd.lang.java.util.namelist.NameListConfig;
 
-import net.sourceforge.pmd.lang.ast.Node;
-import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceType;
-import net.sourceforge.pmd.lang.java.ast.ASTLocalVariableDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTName;
-import net.sourceforge.pmd.lang.java.ast.ASTPrimitiveType;
-import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclaratorId;
-import org.jaxen.JaxenException;
+import net.sourceforge.pmd.lang.ast.NodeStream;
+import net.sourceforge.pmd.lang.java.ast.*;
+import net.sourceforge.pmd.lang.java.types.JPrimitiveType;
+import net.sourceforge.pmd.lang.java.types.JTypeMirror;
 
 /**
  * [Recommended] If the return type is primitive, return a value of wrapper class may cause NullPointerException.
  *
- * @author changle.lq
- * @date 2017/04/16
+ * @author XiNing.Liu
+ * @date 2025/03/12
  */
 public class MethodReturnWrapperTypeRule extends AbstractAliRule {
-    private static final Map<String, String> PRIMITIVE_TYPE_TO_WAPPER_TYPE = NameListConfig.NAME_LIST_SERVICE
-        .getNameMap("MethodReturnWrapperTypeRule", "PRIMITIVE_TYPE_TO_WAPPER_TYPE", String.class, String.class);
-    private static final String METHOD_RETURN_TYPE_XPATH = "ResultType/Type/PrimitiveType";
-    private static final String METHOD_RETURN_OBJECT_XPATH
-        = "Block/BlockStatement/Statement/ReturnStatement/Expression/PrimaryExpression/PrimaryPrefix/Name";
-    private static final String METHOD_VARIABLE_TYPE_XPATH = "Type/ReferenceType/ClassOrInterfaceType";
-    private static final String METHOD_VARIABLE_NAME_XPATH
-        = "Block/BlockStatement/LocalVariableDeclaration/VariableDeclarator/VariableDeclaratorId";
 
     @Override
-    public Object visit(ASTMethodDeclaration node, Object data) {
-        try {
-            List<Node> astPrimitiveTypeList = node.findChildNodesWithXPath(METHOD_RETURN_TYPE_XPATH);
-            //check the method return type
-            if (!(astPrimitiveTypeList != null && astPrimitiveTypeList.size() == 1)) {
-                return super.visit(node, data);
-            }
-            ASTPrimitiveType astPrimitiveType = (ASTPrimitiveType)astPrimitiveTypeList.get(0);
-            //If the return type is not a basic types,skip
-            if (!(astPrimitiveType.getType() != null && astPrimitiveType.getType().isPrimitive())) {
-                return super.visit(node, data);
-            }
-            //the return type
-            String primitiveTypeName = astPrimitiveType.getType().getName();
-            //the return node
-            List<Node> nameList = node.findChildNodesWithXPath(METHOD_RETURN_OBJECT_XPATH);
-            if (nameList == null || nameList.size() != 1) {
-                return super.visit(node, data);
-            }
-            //if the local variable is empty,skip
-            List<Node> methodVariableNameList = node.findChildNodesWithXPath(METHOD_VARIABLE_NAME_XPATH);
-            if (methodVariableNameList == null || methodVariableNameList.size() == 0) {
-                return super.visit(node, data);
-            }
-            ASTName astName = (ASTName)nameList.get(0);
-            String variableName = astName.getImage();
-            //iterate all the method of variable nodes
-            for (Node methodVariableNameNode : methodVariableNameList) {
-                ASTVariableDeclaratorId astVariableDeclaratorId
-                    = (ASTVariableDeclaratorId)methodVariableNameNode;
-                //find out the variable named the same with return node
-                if (!variableName.equals(astVariableDeclaratorId.getImage())) {
-                    continue;
-                }
-                ASTLocalVariableDeclaration astLocalVariableDeclaration = astVariableDeclaratorId
-                    .getFirstParentOfType(ASTLocalVariableDeclaration.class);
-                //check local variables type
-                List<Node> nodeList = astLocalVariableDeclaration.findChildNodesWithXPath(
-                    METHOD_VARIABLE_TYPE_XPATH);
+    public Object visit(ASTMethodDeclaration methodNode, Object data) {
 
-                if (nodeList != null && nodeList.size() == 1) {
-                    ASTClassOrInterfaceType astClassOrInterfaceType = (ASTClassOrInterfaceType)nodeList.get(
-                        0);
-                    //if variable type is a value of wrapper
-                    if (PRIMITIVE_TYPE_TO_WAPPER_TYPE.get(primitiveTypeName) != null
-                        && PRIMITIVE_TYPE_TO_WAPPER_TYPE.get(primitiveTypeName).equals(
-                        astClassOrInterfaceType.getType().getSimpleName())) {
-                        ViolationUtils.addViolationWithPrecisePosition(this, node, data,
-                            I18nResources.getMessage("java.exception.MethodReturnWrapperTypeRule.violation.msg",
-                                primitiveTypeName, astClassOrInterfaceType.getType().getSimpleName()));
-                    }
-                }
-            }
-        } catch (JaxenException e) {
-            return super.visit(node, data);
+        /*
+          public static int getNumberWrapper() {
+        Integer number = null; // 故意将 Integer 设置为 null
+        return number; // 尝试返回 null 的 Integer 给 int 返回类型，这里会触发自动拆箱
+               }
+         */
+        // Find the primitive return type
+        ASTPrimitiveType primitiveTypeOptional = methodNode.descendants(ASTType.class)
+                .flatMap(type -> type.descendants(ASTPrimitiveType.class))
+                .first();
+
+
+        if (Objects.isNull(primitiveTypeOptional)) {
+            return super.visit(methodNode, data);
         }
-        return super.visit(node, data);
+
+
+        JPrimitiveType.PrimitiveTypeKind kind = primitiveTypeOptional.getKind();
+        // int char
+        String returnTypeName = kind.getSimpleName();
+
+
+        // Find the return statements with a variable name
+        List<ASTReturnStatement> methodReturnNodeList
+                = methodNode.descendants(ASTBlock.class).flatMap(block -> block.descendants(ASTReturnStatement.class)).toList();
+
+        if (methodReturnNodeList.isEmpty()) {
+            return super.visit(methodNode, data);
+        }
+        // all variable name, ["name","text","age"]
+        List<ASTVariableId> astVariableIdList = methodNode.descendants(ASTLocalVariableDeclaration.class).descendants(ASTVariableId.class).toList();
+        List<String> variableNameList = astVariableIdList.stream().map(ASTVariableId::getName).collect(Collectors.toList());
+
+        for (ASTReturnStatement returnStatement : methodReturnNodeList) {
+
+            ASTVariableAccess astVariableAccess = returnStatement.descendants(ASTPrimaryExpression.class).descendants(ASTVariableAccess.class).first();
+
+            if (Objects.nonNull(astVariableAccess)) {
+                // var name like "text" in  String text = "123"; return text;
+                String name = astVariableAccess.getName();
+                if (variableNameList.contains(name)) {
+                    // Find the variable type, like text is String
+                    ASTLocalVariableDeclaration firstLocalDecl = astVariableAccess.ancestors(ASTLocalVariableDeclaration.class).first();
+                    if (Objects.nonNull(firstLocalDecl)) {
+                        ASTType typeNode = firstLocalDecl.getTypeNode();
+                        if (Objects.nonNull(typeNode)) {
+                            JTypeMirror typeMirror = typeNode.getTypeMirror();
+                            boolean classOrInterface = typeMirror.isClassOrInterface();
+                            if (classOrInterface && Objects.nonNull(typeMirror.getSymbol())) {
+                                ViolationUtils.addViolationWithPrecisePosition(this, methodNode, data,
+                                        I18nResources.getMessage("java.exception.MethodReturnWrapperTypeRule.violation.msg",
+                                                returnTypeName, typeMirror.getSymbol().getSimpleName()));
+                            }
+                        }
+                    }
+
+                }
+
+            }
+
+        }
+
+        return super.visit(methodNode, data);
     }
 }
