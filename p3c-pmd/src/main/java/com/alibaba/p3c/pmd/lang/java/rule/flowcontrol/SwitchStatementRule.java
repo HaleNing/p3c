9d198d0ae5caccce8eae28901baf73c13549db89
justinLiu <xining.l.se@gmail.com>
2025-03-16 21:58:18 +0800

    Refactor SwitchStatementRule to improve operator checking logic and support PMD7

--- p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/flowcontrol/SwitchStatementRule.java
+++ p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/flowcontrol/SwitchStatementRule.java
@@ -1,31 +1,16 @@
-/*
- * Copyright 1999-2017 Alibaba Group.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.alibaba.p3c.pmd.lang.java.rule.flowcontrol;
 
 import com.alibaba.p3c.pmd.lang.java.rule.AbstractAliRule;
-
-import net.sourceforge.pmd.lang.java.ast.ASTSwitchStatement;
+import net.sourceforge.pmd.lang.java.ast.*;
+import java.util.List;
 
 /**
  * [Mandatory] In a switch block, each case should be finished by break/return.
  * If not, a note should be included to describe at which case it will stop. Within every switch block,
  * a default statement must be present, even if it is empty.
  *
- * @author zenghou.fw
- * @date 2016/11/17
+ * @author XiNing.Liu
+ * @date 2025/03/16
  */
 public class SwitchStatementRule extends AbstractAliRule {
     private static final String MESSAGE_KEY_PREFIX = "java.flowcontrol.SwitchStatementRule.violation";
@@ -33,50 +18,79 @@ public class SwitchStatementRule extends AbstractAliRule {
     @Override
     public Object visit(ASTSwitchStatement node, Object data) {
         checkDefault(node, data);
-
         checkFallThrough(node, data);
-
         return super.visit(node, data);
     }
 
     /**
      * Check if switch statement contains default branch
-     *
-     * @param node
-     * @param data
      */
     private void checkDefault(ASTSwitchStatement node, Object data) {
-        final String switchCheckXpath = "SwitchLabel[@Default = 'true']";
-        if (!node.hasDescendantMatchingXPath(switchCheckXpath)) {
+        // Check if there's a default label in the switch statement
+        boolean notDefault = node.descendants(ASTSwitchLabel.class)
+                .filter(ASTSwitchLabel::isDefault)
+                .count() <= 0;
+        if (notDefault) {
             addViolationWithMessage(data, node, MESSAGE_KEY_PREFIX + ".nodefault");
         }
     }
 
     /**
      * Check the availability of break, return, throw, continue in case statement
-     *
-     * @param node
-     * @param data
      */
     private void checkFallThrough(ASTSwitchStatement node, Object data) {
-        // refer the rule MissingBreakInSwitch of PMD
-        final String xpath = "../SwitchStatement[(count(.//BreakStatement)"
-            + " + count(BlockStatement//Statement/ReturnStatement)"
-            + " + count(BlockStatement//Statement/ContinueStatement)"
-            + " + count(BlockStatement//Statement/ThrowStatement)"
-            + " + count(BlockStatement//Statement/IfStatement[@Else='true'"
-            + " and Statement[2][ReturnStatement|ContinueStatement|ThrowStatement]]"
-            + "/Statement[1][ReturnStatement|ContinueStatement|ThrowStatement])"
-            + " + count(SwitchLabel[name(following-sibling::node()) = 'SwitchLabel'])"
-            + " + count(SwitchLabel[count(following-sibling::node()) = 0])"
-            + "  < count (SwitchLabel[@Default != 'true'])"
-            + " + count(SwitchLabel[@Default = 'true']/following-sibling::BlockStatement//Statement/ReturnStatement)"
-            + " + count(SwitchLabel[@Default = 'true']/following-sibling::BlockStatement//Statement/ContinueStatement)"
-            + " + count(SwitchLabel[@Default = 'true']/following-sibling::BlockStatement//Statement/ThrowStatement)"
-            + ")]";
+        // Count all case labels (excluding default)
+        int caseLabelsCount = node.descendants(ASTSwitchLabel.class)
+                .filter(label -> !label.isDefault())
+                .count();
 
-        if (node.hasDescendantMatchingXPath(xpath)) {
+        // Count all termination statements
+        int terminatingStatementsCount = 0;
+
+        // Count break statements
+        terminatingStatementsCount += node.descendants(ASTBreakStatement.class).count();
+
+        // Count return, continue, throw statements in block statements
+        terminatingStatementsCount += node.descendants(ASTBlock.class)
+                .flatMap(block -> block.descendants(ASTStatement.class))
+                .filter(stmt -> {
+                    return stmt.descendants(ASTReturnStatement.class).count() > 0 ||
+                            stmt.descendants(ASTContinueStatement.class).count() > 0 ||
+                            stmt.descendants(ASTThrowStatement.class).count() > 0;
+                })
+                .count();
+
+        // Count empty cases (a case followed immediately by another case or default)
+        terminatingStatementsCount += countEmptyCases(node);
+
+        // If there are fewer terminating statements than case labels, some cases fall through
+        if (terminatingStatementsCount < caseLabelsCount) {
             addViolationWithMessage(data, node, MESSAGE_KEY_PREFIX + ".notermination");
         }
+    }
+
+    /**
+     * Count cases that are empty (followed directly by another case or are the last case)
+     */
+    private int countEmptyCases(ASTSwitchStatement node) {
+        int emptyCount = 0;
+
+        // Get all the nodes in the switch statement body
+        List<JavaNode> list = node.children().toList();
+
+        // Check for consecutive labels
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) instanceof ASTSwitchLabel &&
+                    list.get(i + 1) instanceof ASTSwitchLabel) {
+                emptyCount++;
+            }
+        }
+
+        // Check if the last child is a switch label (empty last case)
+        if (!list.isEmpty() && list.get(list.size() - 1) instanceof ASTSwitchLabel) {
+            emptyCount++;
+        }
+
+        return emptyCount;
     }
 }
